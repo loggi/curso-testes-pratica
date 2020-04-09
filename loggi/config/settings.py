0c9a86e02f433274f30691313b9f1f9978f59565
Ana <ee.anap@gmail.com>
2020-04-09 07:47:14 -0300

    add messaging in settings

--- loggi/config/settings.py
+++ loggi/config/settings.py
@@ -21,13 +21,16 @@ LOCAL_APPS = [
     'dispatch',
     'examples',
     'players',
+    'messaging'
 ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
